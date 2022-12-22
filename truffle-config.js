8e692088a434517b58e691a63cba95e72e421cb7
Sagun Panthi <sagunpanthi@Saguns-MacBook-Air.local>
2022-12-22 16:49:54 +1100

    initial commit

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+/* const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "hedgehog protect ladder dune wave supreme shove trial tiger victory ghost know"; */
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -57,14 +58,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+   /*  goerli: {
+    provider: () => new HDWalletProvider(mnemonic, `https://eth-goerli.g.alchemy.com/v2/Lm1Qr1CojVA5NnU9S0UV0Ok7oVTvTYF-`),
+    network_id: 5,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }, */
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
